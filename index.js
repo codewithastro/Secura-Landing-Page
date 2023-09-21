b228efed26d504f11d45e5e16b06737be76a18a1
codewithastro <amoriamori3016@gmail.com>
2023-09-22 00:12:09 +0200

    new cards design new social icons

---
+++ index.js
@@ -0,0 +1,4 @@
+//this project doesn't contain any JS scripts
+document.getElementById("what").addEventListener("click"),function(){
+    alert("Come on Man you will see me in every project :)")
+}  
